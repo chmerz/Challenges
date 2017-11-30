67785c7a39a71b6b329db1f6278b1f4b3536f1bd
learn <learn@learn11.local>
2017-11-29 16:56:37 -0800

    remove day 3 folder

---
+++ iterations.js
@@ -0,0 +1,55 @@
+Math.max (2,4,6,7,8)
+var number = [2,5,1,70]
+var test = function(a) {
+    console.log (a)
+    a*a
+}
+
+number.forEach(function(a){
+  console.log(a*a)
+})
+
+Math.min (2,4,6,7,8)
+
+
+
+
+var numArray = [3,10,54,9];
+numArray.sort(function(a, b) {
+        return a - b
+    }
+);
+numArray [0]
+
+
+
+
+var sum = [1,2,3].reduce(add, 0);
+function add (a, b) {
+    return a + b;
+}
+sum
+
+
+
+//while loop challenge
+var i = 0;
+while (i < 10) {
+    i++;
+    console.log (i);
+}
+
+
+
+
+
+
+
+// this is for assignment
+var newthing = []
+
+// this is for comparison
+number1 == number2
+
+// check for absolute equality
+number1 === number2
